0b06807f7bce45a178c7a9f82d5da0445aa518ee
Sahanagc6 <149249486+Sahanagc6@users.noreply.github.com>
2024-01-02 19:26:37 +0530

    Update main.dart

--- ToDo Flutter/lib/main.dart
+++ ToDo Flutter/lib/main.dart
@@ -25,36 +25,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// This is the widget that will be shown
-// as the homepage of your application.
-class GFG extends StatefulWidget {
-  const GFG({Key? key}) : super(key: key);
 
-  @override
-  State<GFG> createState() => _GFGState();
-}
-
-class _GFGState extends State<GFG> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-    appBar:
-    AppBar(
-      title: const Text(
-        "GeeksForGeeks",
-      ),
-      actions: [
-        IconButton(
-          onPressed: () {
-            // method to show the search bar
-            showSearch(
-                context: context,
-                // delegate to customize the search bar
-                delegate: CustomSearchDelegate());
-          },
-          icon: const Icon(Icons.search),
-        )
-      ],
-    );
-  }
-}
+     
+  
